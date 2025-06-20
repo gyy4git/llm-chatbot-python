321626123b96266288f8f03cf87844e67f4b157b
gyy4git <133186006+gyy4git@users.noreply.github.com>
2025-06-20 23:57:33 +0000

    update

--- llm.py
+++ llm.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
 # Create the LLM
+openai_api_key = st.secrets['OPENAI_API_KEY']
+openai_model = st.secrets['OPENAI_MODEL']
 
+# Create the LLM
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
 
 # Create the Embedding model
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
+
